3792c64d2ba5ab8949463ebcd2587cbe5c7c8f75
hojong2 <bcpjh8624@naver.com>
2022-10-17 16:40:49 +0900

    역할분담 1

---
+++ tetris.js
@@ -0,0 +1,21 @@
+function setTable(){
+    let table="<table id=\"gametable\">";
+    for(var i=0; i<30; i++){ 
+        table+="<tr>";
+        for(var j=0; j<20; j++){ 
+            if(j===0 || j===19 || i===0 || i===29){
+                table+="<td style=\"background:#16293f; width:16px; height:16px\" id=\""+String(i)+" "+String(j)+"\"></td>";  //아이디 값을 배열 형태로 지정
+
+            }else{
+                table+="<td style=\"background:black; width:16px; height:16px id=\""+String(i)+" "+String(j)+"\"></td>";
+            }
+        }
+        table+="</tr>";
+    }
+    table+="</table>";
+
+    document.write(table);
+}
+(function(){
+    setTable();
+})()
