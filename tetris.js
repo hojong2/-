08d480c927adfff6098e35274d666bc699360e7d
hojong2 <bcpjh8624@naver.com>
2022-10-17 16:56:42 +0900

    백틱 변경

--- tetris.js
+++ tetris.js
@@ -3,11 +3,11 @@ function setTable(){
     for(var i=0; i<30; i++){ 
         table+="<tr>";
         for(var j=0; j<20; j++){ 
-            if(j===0 || j===19 || i===0 || i===29){
-                table+="<td style=\"background:#16293f; width:16px; height:16px\" id=\""+String(i)+" "+String(j)+"\"></td>";  //아이디 값을 배열 형태로 지정
+            if(j===0 || j===19 || i===0 || i===29){0
+                table+=`<td style="background:#16293f; width:16px; height:16px" id="${String(i)} ${String(j)}"></td>`;  //아이디 값을 배열 형태로 지정
 
             }else{
-                table+="<td style=\"background:black; width:16px; height:16px id=\""+String(i)+" "+String(j)+"\"></td>";
+                table+=`<td style="background:black; width:16px; height:16px" id="${String(i)} ${String(j)}"></td>`;
             }
         }
         table+="</tr>";
@@ -16,6 +16,11 @@ function setTable(){
 
     document.write(table);
 }
+function fallblock(){
+    let block = document.getElementById("1 10");
+    block.style.background="white";
+}
 (function(){
     setTable();
+    fallblock();
 })()
